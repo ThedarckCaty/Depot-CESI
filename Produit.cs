6c7450dfe910e3e49c826b03eb1cdcdda8c3d358
ThedarckCaty <156935702+ThedarckCaty@users.noreply.github.com>
2024-01-24 16:34:22 +0100

    Update Produit.cs

--- Produit.cs
+++ Produit.cs
@@ -13,14 +13,6 @@ namespace Entreprise
         public string Nom { get; set; }
         public float Prix { get; set; }
         public Fournisseur Fournisseur { get; set; }
-
-        // Autres propriétés et méthodes spécifiques à la classe Produit
-
-        public void Vendre()
-        {
-            // Logique de vente spécifique pour un produit
-            Console.WriteLine($"Le produit {Nom} a été vendu !");
-        }
     }
 
 
